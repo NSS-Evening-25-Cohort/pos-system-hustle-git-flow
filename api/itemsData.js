a24f22a78cc55ed03f0213f27bcd6ed0b4d59caa
Bruce Clark <Bruce.clark55@gmail.com>
2023-12-11 22:41:21 -0500

    added items data

---
+++ api/itemsData.js
@@ -0,0 +1,84 @@
+import client from "../utils/client";
+
+const endpoint = client.databaseURL;
+
+//TODO: GET Items
+const getItems = (cid) => new Promise((resolve, reject) => {
+    fetch(`${endpoint}/items.json?orderBy"cid"andequalTo="${cid}"`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          resolve(Object.values(data));
+        } else {
+          resolve([]);
+        }
+      })
+      .catch(reject);
+  });
+
+//TODO: DELETE Items
+const deleteSingleItems = (firebaseKey) => new Promise((resolve, reject) => {
+    fetch(`${endpoint}/items/${firebaseKey}.json`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+//TODO: CREATE Items
+const createItems = (payload) => new Promise((resolve, reject) => {
+    fetch(`${endpoint}/items.json`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+//TODO: UPDATE Items
+const updateItems = (payload) => new Promise((resolve, reject) => {
+    fetch(`${endpoint}/items/${payload.firebaseKey}.json`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+//TODO: GET SINGLE Items
+const getSingleItems = (firebaseKey) => new Promise((resolve, reject) => {
+    fetch(`${endpoint}/items/${firebaseKey}.json`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+  export {
+    createItems,
+    getSingleItems,
+    getItems,
+    deleteSingleItems,
+    createItems,
+    updateItems
+  }
