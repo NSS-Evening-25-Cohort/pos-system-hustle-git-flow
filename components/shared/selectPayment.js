beef49fcb4f81d1d962bd1a8d0f2e59c3a819fb4
Daun Kim <yennakim11@gmail.com>
2023-12-12 19:51:55 -0600

    corrected dropdown values  within selectPayment() function

--- components/shared/selectPayment.js
+++ components/shared/selectPayment.js
@@ -4,8 +4,9 @@ const selectPayment = () => {
   const domString = `<label for="paymentType">Payment Type</label>
   <select class="form-control" id="paymentType" required>
   <option selected">Select a Payment Type</option>
-  <option value="Coffee">Coffee</option>
-  <option value="Pastry">Pastry</option>
+  <option value="cash">Cash</option>
+  <option value="card">card</option>
+  <option value="mobile">mobile</option>
   </select>`;
   renderToDOM('#select-payment', domString);
 };
