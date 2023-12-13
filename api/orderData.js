19c515378f24de4c87c9ce9a5b33aad9c017b5ce
Bruce Clark <Bruce.clark55@gmail.com>
2023-12-12 19:38:02 -0500

    added create order

--- api/orderData.js
+++ api/orderData.js
@@ -1,13 +1,54 @@
-// import client from "../utils/client";
+import client from '../utils/client';
 
-// const endpoint = client.databaseURL;
+const endpoint = client.databaseURL;
 
 // TODO: GET Order
 
 // TODO: DELETE Order
+const deleteSingleOrder = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/order/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
 
 // TODO: CREATE Order
-
+const createOrder = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/order.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
 // TODO: UPDATE Order
+const updateOrder = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/order/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
 
 // TODO: GET SINGLE Order
+
+export {
+  deleteSingleOrder,
+  createOrder,
+  updateOrder,
+
+};
