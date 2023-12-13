d05370e456ddc63ae9461cb241aeaa67b6eab4ee
Bruce Clark <Bruce.clark55@gmail.com>
2023-12-12 19:41:11 -0500

    added all of orderdata

--- api/orderData.js
+++ api/orderData.js
@@ -3,7 +3,23 @@ import client from '../utils/client';
 const endpoint = client.databaseURL;
 
 // TODO: GET Order
-
+const getOrder = (cid) => new Promise((resolve, reject) => {
+    fetch(`${endpoint}/order.json?orderBy"cid"andequalTo="${cid}"`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          resolve(Object.values(data));
+        } else {
+          resolve([]);
+        }
+      })
+      .catch(reject);
+  });
 // TODO: DELETE Order
 const deleteSingleOrder = (firebaseKey) => new Promise((resolve, reject) => {
   fetch(`${endpoint}/order/${firebaseKey}.json`, {
@@ -45,10 +61,22 @@ const updateOrder = (payload) => new Promise((resolve, reject) => {
 });
 
 // TODO: GET SINGLE Order
+const getSingleOrder = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/order/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
 
 export {
   deleteSingleOrder,
   createOrder,
   updateOrder,
-
+  getSingleOrder,
+  getOrder
 };
