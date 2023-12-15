b2d2491ee81231f2e51580ef45004e7ebd19e1cf
Daun Kim <yennakim11@gmail.com>
2023-12-14 20:08:46 -0600

    added logo to sign-in screen

--- utils/clearDom.js
+++ utils/clearDom.js
@@ -1,4 +1,5 @@
 const clearDom = () => {
+  document.querySelector('#login-form-container').innerHTML = '';
   document.querySelector('#store').innerHTML = '';
   document.querySelector('#add-button').innerHTML = '';
   document.querySelector('#form-container').innerHTML = '';
