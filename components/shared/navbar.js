4082012806e49709957ccf196b6c075ba4e6517b
Daun Kim <yennakim11@gmail.com>
2023-12-14 20:00:15 -0600

    replaced navbar brand title with logo

--- components/shared/navbar.js
+++ components/shared/navbar.js
@@ -1,10 +1,11 @@
 import renderToDOM from '../../utils/renderToDom';
+import logo from '../../assets/logo.png';
 
 const navBar = () => {
   const domString = `
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-5">
     <div class="container-fluid">
-        <a class="navbar-brand title" id="home" href="#">Hip Hop Pizza</a>
+        <a class="navbar-brand title" id="home" href="#"><img src="${logo}" alt="Hip Hop Pizza logo" width="50" height="50"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
