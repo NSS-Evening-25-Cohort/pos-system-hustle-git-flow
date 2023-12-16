ba3bc36a4dcfdccccdb4f2c5bf5c0da7f22da369
unknown <hautersolomon@gmail.com>
2023-12-16 09:41:46 -0600

    tsdasd

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,8 +1,9 @@
 import {
-  createOrder, deleteSingleOrder, getOrder, getSingleOrder
+  createOrder, deleteSingleOrder, getOrder,
 } from '../api/orderData';
 import createEditOrder from '../components/form/createEditOrder';
 import viewOrders from '../pages/viewOrder';
+import { getSingleCustomer } from '../api/customerData';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -23,10 +24,21 @@ const domEvents = () => {
     }
     // EDIT ORDER
     if (e.target.id.includes('edit-order')) {
-      console.warn('LOOK AT ME');
-      const [, firebaseKey] = e.target.id.split('--');
+      const [, firebaseKey] = e.target.id.split('--') || [];
 
-      getSingleOrder(firebaseKey).then((orderObj) => createEditOrder(orderObj));
+      if (firebaseKey) {
+        getSingleCustomer(firebaseKey).then((orderObj) => {
+          if (orderObj) {
+            createEditOrder(orderObj);
+          } else {
+            console.error('Order object not found.');
+          }
+        }).catch((error) => {
+          console.error('Error fetching order:', error);
+        });
+      } else {
+        console.error('Firebase key not found.');
+      }
     }
     // // DELETE ITEM
     // if (e.target.id.includes('delete-item')) {
