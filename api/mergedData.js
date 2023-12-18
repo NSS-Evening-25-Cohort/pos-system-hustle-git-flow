149d4b6ea2c74d0fd8077e4183735625edc8a616
Daun Kim <yennakim11@gmail.com>
2023-12-18 00:18:28 -0600

    working on mergedData and orderDetails; issue is with array.forEach

--- api/mergedData.js
+++ api/mergedData.js
@@ -4,8 +4,7 @@ import { getSingleOrder } from './orderData';
 // TODO: GET ORDER DETAILS
 const getOrderDetails = (firebaseKey) => new Promise((resolve, reject) => {
   getSingleCustomer(firebaseKey).then((customerObject) => {
-    getSingleOrder(customerObject.order_id)
-      .then((orderObject) => resolve({ ...customerObject, orderObject }));
+    getSingleOrder(firebaseKey).then((orderObject) => resolve({ ...customerObject, orderObject }));
   }).catch(reject);
 });
 
--- pages/OrderDetails.js
+++ pages/OrderDetails.js
@@ -1,23 +1,22 @@
 import renderToDOM from '../utils/renderToDOM';
 import clearDom from '../utils/clearDom';
 
-const orderDetails = (obj) => {
+const orderDetails = (array) => {
   clearDom();
   let domString = `<div><h1>TOTAL:</h1></div>
-  <div class="mt-5 d-flex flex-wrap";`;
+  <div class="mt-5 d-flex flex-wrap">`;
   let domStringCard = '';
-  obj.customerObject.forEach((item) => {
+  array.customerObject.forEach((item) => {
     domStringCard += `<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">${item.order}</h5>
-    <h5 class="card-title">$${item.orderTotal}</h5>
-    <a href="#" id="edit-item--${item.firebaseKey}" class="card-link">EDIT</a>
-    <a href="#" id="delete-item--${item.firebaseKey}" class="card-link">DELETE</a>
-  </div>
-</div>`;
+    <div class="card-body">
+      <h5 class="card-title">${item.order}</h5>
+      <h5 class="card-title">$${item.orderTotal}</h5>
+      <a href="#" id="edit-item--${item.firebaseKey}" class="card-link">EDIT</a>
+      <a href="#" id="delete-item--${item.firebaseKey}" class="card-link">DELETE</a>
+    </div>
+  </div>`;
   });
   domString += domStringCard;
-
   renderToDOM('#view', domString);
 };
 
