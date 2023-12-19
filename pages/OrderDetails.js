5bb1869108801547fe5a4aaa6a2e67175cdf44a2
unknown <hautersolomon@gmail.com>
2023-12-18 19:11:51 -0600

    this is the commit

--- pages/OrderDetails.js
+++ pages/OrderDetails.js
@@ -1,20 +1,24 @@
 import renderToDOM from '../utils/renderToDOM';
 import clearDom from '../utils/clearDom';
 
-const orderDetails = (object) => {
+const orderDetails = (orderObject) => {
   clearDom();
-
-  // Check if object and orderObject are defined
 
   const domString = `<div class="card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title">Order: ${object.order}</h5>
-      <h5 class="card-title">Order Total: asd</h5>
-      <a href="#" id="edit-order-${object.firebaseKey}" class="card-link">Edit</a>
-      <a href="#" id="deleteorder-${object.firebaseKey}" class="card-link">Delete</a>
-    </div>`;
+      <h5 class="card-header">Order: ${orderObject.itemName}</h5>
+      <p class="card-text">Cust Phone: ${orderObject.customerPhone}</p>
+      <a href="#" id="edit-item--${orderObject.firebaseKey}" class="card-link">Edit</a>
+      <a href="#" id="delete-item--${orderObject.firebaseKey}" class="card-link">Delete</a>
+    </div>
+  </div>
+  <div><h1>TOTAL:</h1></div>
+  <div class="mt-5 d-flex flex-wrap">
+  <hr>
+  <button type="button" id="add-item" class="btn btn-success">Add Item</button>
+  <button type="button" id="paymentPage-btn" class="btn btn-primary">Go to Payment</button>
+  </div>`;
 
-  // domString += domStringCard;
   renderToDOM('#view', domString);
 };
 
